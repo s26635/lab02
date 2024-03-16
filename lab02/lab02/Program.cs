8be1535586ff571c06b50c96ecf7cdaaed82b4ac
s26635 <s26635@pjwstk.edu.pl>
2024-03-16 16:07:35 +0100

    Dodanie metody liczacej srednia

--- lab02/lab02/Program.cs
+++ lab02/lab02/Program.cs
@@ -3,5 +3,14 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Branch nr 2");
+        
+        int[] tab = { 32, 12, 5, 65, 15 };
+        
+        Console.WriteLine(AvgVal(tab));
+    }
+
+    public static double AvgVal(int[] tab)
+    {
+        return tab.Average();
     }
 }
