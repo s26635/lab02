63101f1659e27172a8c1ebdf2fd91c29420c96c7
s26635 <s26635@pjwstk.edu.pl>
2024-03-16 16:19:07 +0100

    Dodanie metody MaxVal

--- lab02/lab02/Program.cs
+++ lab02/lab02/Program.cs
@@ -5,8 +5,14 @@
         Console.WriteLine("Test commit z domu");
         Console.WriteLine("Test");
 
-        int[] tab = new[] { 32, 12, 5, 65, 15 };
+        int[] tab = { 32, 12, 5, 65, 15 };
+        Console.WriteLine(AvgVal(tab));
         Console.WriteLine(MaxVal(tab));
+    }
+
+    public static double AvgVal(int[] tab)
+    {
+        return tab.Average();
     }
 
     public static int MaxVal(int[] tab)
